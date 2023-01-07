d115837ee40c6203b7ce009a573a4f2ffc704833
BiancaGabriela06 <bianca-gabriela.asavoaei@s.unibuc.ro>
2023-01-07 21:25:30 +0200

    user

---
+++ Data/Migrations/20230107192240_Migratie07.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenDiscussionAutentificareIdentity.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migratie07 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
