c0408fc847dce00bee1aedb5eaac85260f68711b
BiancaGabriela06 <bianca-gabriela.asavoaei@s.unibuc.ro>
2023-02-18 22:20:23 +0200

    Editare user pentru admin

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OpenDiscussion_AutentificareIdentity.Data;
 using OpenDiscussion_AutentificareIdentity.Models;
 
@@ -33,8 +34,79 @@ namespace OpenDiscussionPlatform.Controllers
             return View();
         }
 
-        // SHOW
-        public async Task<ActionResult> Show(string id)
+
+		public async Task<ActionResult> Edit(string id)
+		{
+			AppUser user = db.Users.Find(id);
+            user.AllRoles = GetAllRoles();
+            var roleNames = await _userManager.GetRolesAsync(user); // lista de nume de roluri
+
+            var currentUserRolse = _roleManager.Roles
+                                               .Where(role => roleNames.Contains(role.Name))
+                                               .Select(role => role.Id)
+                                               .First();    /// se selecteaza un singur rol
+            ViewBag.UserRole = currentUserRolse;
+            return View(user);
+		}
+
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(string id, AppUser newUser, [FromForm] string newRole)
+        {
+            AppUser user = db.Users.Find(id);
+            user.AllRoles = GetAllRoles();
+
+            if(ModelState.IsValid)
+            {
+                user.UserName = newUser.UserName;
+                user.Email = newUser.Email;
+                user.FirstName = newUser.FirstName;
+                user.LastName = newUser.LastName;
+                user.PhoneNumber = newUser.PhoneNumber;
+
+                var roles = db.Roles.ToList();
+                foreach(var role in roles)
+                {
+                    //Scoatem userul din rolurile anterioare
+                    await _userManager.RemoveFromRoleAsync(user, role.Name);
+
+                }
+
+                ///Adaugam noul rol selectat
+                var roleName = await _roleManager.FindByIdAsync(newRole);
+                await _userManager.AddToRoleAsync(user, roleName.ToString());
+                db.SaveChanges();
+
+
+            }
+            return RedirectToAction("Index");
+
+
+        }
+
+
+		[NonAction]
+		public IEnumerable<SelectListItem> GetAllRoles()
+		{
+			var selectList = new List<SelectListItem>();
+
+			var roles = from role in db.Roles
+						select role;
+
+			foreach (var role in roles)
+			{
+				selectList.Add(new SelectListItem
+				{
+					Value = role.Id.ToString(),
+					Text = role.Name.ToString()
+				});
+			}
+			return selectList;
+		}
+
+
+		// SHOW
+		public async Task<ActionResult> Show(string id)
         {
             AppUser user = db.AppUsers.Find(id);
             var roles = await _userManager.GetRolesAsync(user);
@@ -65,10 +137,10 @@ namespace OpenDiscussionPlatform.Controllers
 
         // EDIT
 
-        [Authorize(Roles = "Admin")]
-        public IActionResult Edit(string id)
-        {
-            var user = db.Users.FirstOrDefault(x => x.Id == id);
+       // [Authorize(Roles = "Admin")]
+        //public IActionResult Edit(string id)
+        //{
+          /*  var user = db.Users.FirstOrDefault(x => x.Id == id);
             var userRole = db.UserRoles
                                     .Where(ur => ur.UserId == id)
                                     .ToList();
@@ -94,7 +166,7 @@ namespace OpenDiscussionPlatform.Controllers
                 return RedirectToAction("Edit", user.Id);
             }
         }
-
+          */
         // DELETE
 
         [Authorize(Roles = "Admin")]
